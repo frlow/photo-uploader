a9cc9d5866c6fe0975ee23dcecb45643a5a328ef
Fredrik Löwenhamn <fredrik.lowenhamn@ica.se>
2022-08-30 12:29:30 +0200

    First commit

---
+++ src/cli.ts
@@ -0,0 +1,107 @@
+import {
+  Config,
+  getConfig,
+  getImagesToUpload,
+  hasCache,
+  hasConfig,
+  setConfig,
+  updateGDriveCache,
+  uploadImages,
+  validateTargets,
+} from './index'
+import glob from 'glob'
+
+const readline = require('readline')
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+})
+
+const asyncQuestion = (question: string) =>
+  new Promise((resolve: (val: string) => void) => {
+    rl.question(question, function (input: string) {
+      resolve(input)
+    })
+  })
+
+const questionWithFallback = async (
+  question: string,
+  defaultValue: string | undefined
+) => {
+  while (true) {
+    const value =
+      (await asyncQuestion(
+        `${question}${defaultValue ? ` (${defaultValue})` : ''}`
+      )) || defaultValue
+    if (value) return value
+  }
+}
+
+const configureSettings = async () => {
+  const config = getConfig() || ({} as Config)
+  const source = await questionWithFallback('Source dir:', config.source)
+  const target = await questionWithFallback('Target dir:', config.target)
+  const gDriveDir = await questionWithFallback(
+    'Google Drive dir:',
+    config.gDriveDir
+  )
+  const fileTypes = await questionWithFallback('File types', config.fileTypes)
+  return { source, target, gDriveDir, fileTypes }
+}
+
+const validate = async () => {
+  const errors = await validateTargets()
+  if (errors.length > 0) {
+    errors.forEach((er) => console.error(`Error: ${er}`))
+    rl.close()
+    process.exit()
+  }
+}
+
+const loop = async () => {
+  if (!hasCache()) {
+    console.log('Loading Google Drive files...')
+    await updateGDriveCache()
+    console.log('Done!')
+  }
+  if (!hasConfig()) setConfig(await configureSettings())
+
+  const config = getConfig()!
+  const files = await new Promise((resolve) => {
+    glob(config.source + '/**/*', {}, function (re, files) {
+      resolve(files)
+    })
+  })
+  while (true) {
+    const question = `Select command:
+(u) - Update Google Drive Cache
+(c) - Configure settings
+(s) - Sync files
+(q) - Quit
+`
+    const result = await asyncQuestion(question)
+    switch (result) {
+      case 'u':
+        break
+      case 'c':
+        setConfig(await configureSettings())
+        break
+      case 's':
+        await validate()
+        const toUpload = await getImagesToUpload()
+        if (!toUpload) break
+        const response = await asyncQuestion(
+          `Do you want to upload ${toUpload?.length} files? (y/N): `
+        )
+        if (response === 'y')
+          await uploadImages(toUpload, (msg) => console.log(msg))
+        break
+      case 'q':
+        process.exit()
+        break
+      default:
+        break
+    }
+  }
+}
+loop().then(() => rl.close())
---
+++ src/index.ts
@@ -0,0 +1,117 @@
+import { execCommand } from './exec'
+import * as fs from 'fs'
+import glob from 'glob'
+import * as path from 'path'
+import moment from 'moment'
+
+const gDriveCacheFile = 'cache.json'
+type GDriveCacheItem = { name: string; size: number }
+export const hasCache = () => fs.existsSync(gDriveCacheFile)
+export const loadGDriveFiles = async (): Promise<GDriveCacheItem[]> => {
+  const files = await execCommand('rclone ls gdrive:')
+  return files
+    .split('\n')
+    .map((line) => line.trim())
+    .map((line) => line.split(' '))
+    .map((line) => ({ name: line[1], size: parseInt(line[0]) }))
+}
+export const updateGDriveCache = async () => {
+  const files = await loadGDriveFiles()
+  fs.writeFileSync(gDriveCacheFile, JSON.stringify(files, null, 2), 'utf8')
+}
+
+export const getGDriveCache = () => {
+  return fs.existsSync(gDriveCacheFile)
+    ? (JSON.parse(
+        fs.readFileSync(gDriveCacheFile, 'utf8')
+      ) as GDriveCacheItem[])
+    : undefined
+}
+
+export type Config = {
+  source: string
+  target: string
+  gDriveDir: string
+  fileTypes: string
+}
+const configFile = 'config.json'
+export const hasConfig = () => fs.existsSync(configFile)
+export const getConfig = () =>
+  fs.existsSync(configFile)
+    ? (JSON.parse(fs.readFileSync(configFile, 'utf8')) as Config)
+    : undefined
+export const setConfig = (config: Partial<Config>) => {
+  const oldConfig = getConfig()
+  fs.writeFileSync(
+    configFile,
+    JSON.stringify({ ...oldConfig, ...config }, null, 2),
+    'utf8'
+  )
+}
+export const validateTargets = () => {
+  const errors: string[] = []
+  const config = getConfig()
+  if (!config) errors.push('No config found!')
+  if (config) {
+    const targetDir = path.resolve(config.target)
+    if (!fs.existsSync(targetDir)) errors.push('Target dir not found')
+  }
+  return errors
+}
+export const asyncGlob = async (pattern: string) =>
+  new Promise((resolve) => glob(pattern, (er, files) => resolve(files)))
+type UploadItem = { source: string; gDrive?: string; target?: string }
+export const getImagesToUpload = async () => {
+  const config = getConfig()
+  if (!config) return
+  const gDriveFiles = getGDriveCache()
+  if (!gDriveFiles) return
+  const targetFiles = (await asyncGlob(
+    `${path.resolve(config.target)}/**/*`
+  )) as string[]
+  const sourceFiles = (await asyncGlob(`${config.source}/**/*`)) as string[]
+  const toUpload: UploadItem[] = []
+  const fileTypes = config.fileTypes
+    .split(',')
+    .map((ft) => ft.trim().toLowerCase())
+  for (const sourceFile of sourceFiles) {
+    const stat = fs.statSync(sourceFile)
+    const parsed = path.parse(sourceFile)
+    if (fileTypes.includes(parsed.ext.toLowerCase())) {
+      const date = moment(stat.birthtime)
+      const dateFolder = date.format('YYYY-MM-DD')
+      const gDrivePath = path.join(config.gDriveDir, dateFolder, parsed.base)
+      const targetPath = path.join(config.target, dateFolder, parsed.base)
+      const upload: UploadItem = { source: sourceFile }
+      if (!gDriveFiles.some((d) => d.name === gDrivePath))
+        upload.gDrive = gDrivePath
+      if (!fs.existsSync(targetPath)) upload.target = targetPath
+      if (upload.target || upload.gDrive) toUpload.push(upload)
+    }
+  }
+  return toUpload
+}
+
+export const uploadImages = async (
+  toUpload: UploadItem[],
+  log: (msg: string) => void
+) => {
+  const pad = `${toUpload.length}`.length
+  for (const item of toUpload) {
+    log(
+      `(${toUpload.indexOf(item).toString().padStart(pad, '0')}/${
+        toUpload.length
+      }) Processing: ${item.source}`
+    )
+    if (item.gDrive) {
+      console.log('Uploading to GDrive')
+    }
+    if (item.target) {
+      if (!fs.existsSync(path.dirname(item.target)))
+        fs.mkdirSync(path.dirname(item.target), { recursive: true })
+      fs.copyFileSync(item.source, item.target)
+      const stat = fs.statSync(item.source)
+      fs.utimesSync(item.target, stat.atime, stat.mtime)
+    }
+  }
+}
